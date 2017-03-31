62fb46227a4d673115cae0c404c3a12344563913
Piotr Konczak <konczak.piotrek@gmail.com>
2017-03-31 13:28:13 +0200

    init application

---
+++ src/main/java/pl/konczak/learn/mongo/KonczakMongoApplication.java
@@ -0,0 +1,12 @@
+package pl.konczak.learn.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonczakMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonczakMongoApplication.class, args);
+	}
+}
